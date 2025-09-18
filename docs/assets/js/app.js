02a12aaf0011f99485f353041d0adf8ef4f32c3f
Vittor Guilherme Borges de Oliveira <vittoroliveira.dev@gmail.com>
2025-09-18 17:15:46 -0300

    Update app.js

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,9 +1,9 @@
 // assets/js/app.js
-import "./modules/navigation.js";
-import "./modules/consent.js";
-import "./modules/faq-inline.js";
-import "./modules/simples.js";
-import "./modules/about.js";
+import "/corvelloni-final/docs/assets/js/modules/navigation.js";
+import "/corvelloni-final/docs/assets/js/modules/consent.js";
+import "/corvelloni-final/docs/assets/js/modules/faq-inline.js";
+import "/corvelloni-final/docs/assets/js/modules/simples.js";
+import "/corvelloni-final/docs/assets/js/modules/about.js";
 
 const onReady = (fn) =>
   document.readyState === "loading"
@@ -25,7 +25,7 @@ function boot(module, prefer) {
 onReady(() => {
   if (document.querySelector("[data-help-open]")) {
     idle(() =>
-      import("./modules/help-dialog.js")
+      import("/corvelloni-final/docs/assets/js/modules/help-dialog.js")
         .then((m) => m.initHelpDialogs?.())
         .catch((e) => console.error("Falha ao carregar help-dialog.js", e))
     );
@@ -36,7 +36,7 @@ onReady(() => {
 
   if (needCLT) {
     idle(() =>
-      import("./modules/clt-vs-pj.js")
+      import("/corvelloni-final/docs/assets/js/modules/clt-vs-pj.js")
         .then((m) => boot(m, "initCltPjCalculator"))
         .catch((e) => console.error("Falha ao carregar clt-vs-pj.js", e))
     );
@@ -44,7 +44,7 @@ onReady(() => {
 
   if (needCusto) {
     idle(() =>
-      import("./modules/custos-abertura-pj.js")
+      import("/corvelloni-final/docs/assets/js/modules/custos-abertura-pj.js")
         .then((m) => boot(m, "initCustoCnpjCalculator"))
         .catch((e) => console.error("Falha ao carregar custos-abertura-pj.js", e))
     );
